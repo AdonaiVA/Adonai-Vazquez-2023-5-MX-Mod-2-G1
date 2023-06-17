396d4e694d5bcf4edac51124327e9728a0e42e34
Adonai Vázquez Alarcón <125171316+AdonaiVA@users.noreply.github.com>
2023-06-16 20:14:31 -0600

    Tarea 1 - Mejora

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -15,10 +15,26 @@ class Spaceship(Sprite):
         self.speed = 10
 
     def draw(self, screen):
-        screen.blit(self.image, (self.rect.x, SCREEN_HEIGHT - 50))
+        screen.blit(self.image, (self.rect.x, self.rect.y))
 
     def update(self, keyboard_events):
+        self.move_left(keyboard_events)
+        self.move_right(keyboard_events)
+        self.move_up(keyboard_events)
+        self.move_down(keyboard_events)
+        
+    def move_left(self, keyboard_events):   
         if keyboard_events[pygame.K_LEFT] and self.rect.x > 0:
             self.rect.x -= self.speed
-        elif keyboard_events[pygame.K_RIGHT] and self.rect.x < SCREEN_WIDTH - self.width:
+
+    def move_right(self, keyboard_events):
+        if keyboard_events[pygame.K_RIGHT] and self.rect.x < SCREEN_WIDTH - self.width:
             self.rect.x += self.speed
+    
+    def move_up(self, keyboard_events):
+        if keyboard_events[pygame.K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+
+    def move_down(self, keyboard_events):
+        if keyboard_events[pygame.K_DOWN] and self.rect.y < SCREEN_HEIGHT - self.height:
+            self.rect.y += self.speed
